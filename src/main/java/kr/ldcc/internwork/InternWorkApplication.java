d469151f45b07615e7209dfc6d8f93c00bfc648e
seminify <seminify@gmail.com>
2022-10-17 16:53:48 +0900

    add spring-boot-project

---
+++ src/main/java/kr/ldcc/internwork/InternWorkApplication.java
@@ -0,0 +1,13 @@
+package kr.ldcc.internwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternWorkApplication.class, args);
+	}
+
+}
