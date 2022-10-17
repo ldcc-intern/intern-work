dba243566d88d5488160a8ce46c2885acc2a37b1
seminify <seminify@gmail.com>
2022-10-17 17:29:23 +0900

    add user api

---
+++ src/main/java/kr/ldcc/internwork/common/exception/InternWorkException.java
@@ -0,0 +1,19 @@
+package kr.ldcc.internwork.common.exception;
+
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InternWorkException {
+    @NoArgsConstructor
+    public static class dataUpdateException extends RuntimeException {
+    }
+
+    @NoArgsConstructor
+    public static class dataNotFoundException extends RuntimeException {
+    }
+
+    @NoArgsConstructor
+    public static class dataDuplicateException extends RuntimeException {
+    }
+}
---
+++ src/main/java/kr/ldcc/internwork/model/dto/UserDto.java
@@ -0,0 +1,33 @@
+package kr.ldcc.internwork.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Accessors(chain = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(value = JsonInclude.Include.NON_NULL)
+public class UserDto {
+    private Long id;
+    private String name;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @ToString
+    @Accessors(chain = true)
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @JsonInclude(value = JsonInclude.Include.NON_NULL)
+    public static class UserListResponse {
+        private Long id;
+        private String name;
+    }
+}
