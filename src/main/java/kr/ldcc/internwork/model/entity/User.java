7076288f12a91744858845f2c956e888be130af7
seminify <seminify@gmail.com>
2022-10-17 10:43:36 +0900

    add entity

---
+++ src/main/java/kr/ldcc/internwork/model/entity/User.java
@@ -0,0 +1,20 @@
+package kr.ldcc.internwork.model.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
